fc041f05506e44861d39a545ce66a1e02a5142f8
immortius <immortius@gmail.com>
2022-01-22 15:19:02 +1100

    Fabric conversion

---
+++ src/main/java/xyz/immortius/chunkbychunk/fabric/mixins/EntityChangeDimensionMixin.java
@@ -0,0 +1,23 @@
+package xyz.immortius.chunkbychunk.fabric.mixins;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.portal.PortalInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.immortius.chunkbychunk.fabric.ChangeDimensionImpl;
+
+/**
+ * A mixin to enable the ability to change the dimension of entities without using a portal (and to custom dimensions)
+ */
+@Mixin(Entity.class)
+public abstract class EntityChangeDimensionMixin {
+
+    @Inject(method = "findDimensionEntryPoint", at = @At("RETURN"), cancellable = true)
+    private void changeReturnValue(CallbackInfoReturnable<PortalInfo> cir) {
+        if (cir.getReturnValue() == null && ChangeDimensionImpl.getPortalInfo() != null) {
+            cir.setReturnValue(ChangeDimensionImpl.getPortalInfo());
+        }
+    }
+}
--- src/main/java/xyz/immortius/chunkbychunk/interop/CBCInteropMethods.java
+++ src/main/java/xyz/immortius/chunkbychunk/interop/CBCInteropMethods.java
@@ -4,27 +4,38 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.fabricmc.fabric.api.tag.TagFactory;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.portal.PortalInfo;
-import net.minecraftforge.common.util.ITeleporter;
+import net.minecraft.world.level.storage.LevelResource;
+import xyz.immortius.chunkbychunk.config.ChunkByChunkConfig;
+import xyz.immortius.chunkbychunk.config.system.ConfigSystem;
+import xyz.immortius.chunkbychunk.fabric.ChangeDimensionImpl;
+import xyz.immortius.chunkbychunk.fabric.mixins.BucketFluidAccessor;
 
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.List;
 
 /**
  * Static methods whose implementation varies by mod system
  */
 public final class CBCInteropMethods {
+
     private CBCInteropMethods() {
+
     }
 
     /**
      * Change the dimension of an entity.
-     * For forge, this uses the forge extension changeDimension method
+     * For fabric, we use a mixin to enable dimension changes
      *
      * @param entity     The entity to move
      * @param level      The level to move the entity to
@@ -32,7 +43,7 @@ public final class CBCInteropMethods {
      * @return The moved entity
      */
     public static Entity changeDimension(Entity entity, ServerLevel level, PortalInfo portalInfo) {
-        return entity.changeDimension(level, new EntityTeleport(portalInfo));
+        return ChangeDimensionImpl.changeDimension(entity, level, portalInfo);
     }
 
     /**
@@ -40,31 +51,7 @@ public final class CBCInteropMethods {
      * @return The fluid contents of the bucket (may be null)
      */
     public static Fluid getBucketContents(BucketItem bucket) {
-        return bucket.getFluid();
-    }
-
-    private static class EntityTeleport implements ITeleporter {
-
-        private final PortalInfo portalInfo;
-
-        public EntityTeleport(PortalInfo portalInfo) {
-            this.portalInfo = portalInfo;
-        }
-
-        @Override
-        public PortalInfo getPortalInfo(Entity entity, ServerLevel destWorld, Function<ServerLevel, PortalInfo> defaultPortalInfo) {
-            return portalInfo;
-        }
-
-        @Override
-        public boolean isVanilla() {
-            return false;
-        }
-
-        @Override
-        public boolean playTeleportSound(ServerPlayer player, ServerLevel sourceWorld, ServerLevel destWorld) {
-            return false;
-        }
+        return ((BucketFluidAccessor) bucket).getFluid();
     }
 
     /**
